5c985dd1646f306aea1d63b33fcafcd5cfa9ad81
balazs-zsoldos <balazs.zsoldos@everit.biz>
2014-02-03 15:17:43 +0100

    License header replacement

--- src/main/java/org/everit/osgi/dev/maven/result/TestResult.java
+++ src/main/java/org/everit/osgi/dev/maven/result/TestResult.java
@@ -1,18 +1,18 @@
 /**
- * This file is part of Everit Maven OSGi plugin.
+ * This file is part of Everit - Maven OSGi plugin.
  *
- * Everit Maven OSGi plugin is free software: you can redistribute it and/or modify
+ * Everit - Maven OSGi plugin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Everit Maven OSGi plugin is distributed in the hope that it will be useful,
+ * Everit - Maven OSGi plugin is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Everit Maven OSGi plugin.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Everit - Maven OSGi plugin.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.everit.osgi.dev.maven.result;
 
